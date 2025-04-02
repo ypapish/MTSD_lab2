b2f9063ba3a9e3c5b88cb6c17a885dc1ada03256
Yana <y.papish14@gmail.com>
2025-04-02 13:54:12 +0300

    add test_clear method

--- arrDoublyLinkedList_test.py
+++ arrDoublyLinkedList_test.py
@@ -125,3 +125,9 @@ class TestArrayDoublyLinkedList(unittest.TestCase):
         self.assertEqual(self.List.findLast('B'), 1)
         self.assertEqual(self.List.findLast('C'), 3)
         self.assertEqual(self.List.findLast('X'), -1)
+
+    def test_clear(self):
+        self.List.append('A')
+        self.List.append('B')
+        self.List.clear()
+        self.assertEqual(self.List.length(), 0)
