35bcd43c3395df01f3763664e7812595c23082a3
Yana <y.papish14@gmail.com>
2025-04-02 14:46:31 +0300

    add test_insert method

--- doublyLinkedList_test.py
+++ doublyLinkedList_test.py
@@ -17,3 +17,22 @@ class TestDoublyLinkedList(unittest.TestCase):
         self.List.append('B')
         self.assertEqual(self.List.length(), 2)
         self.assertEqual(self.List.get(1), 'B')
+
+    def test_insert(self):
+        self.List.insert('A', 0)
+        self.assertEqual(self.List.get(0), 'A')
+
+        self.List.insert('B', 0)
+        self.assertEqual(self.List.get(0), 'B')
+        self.assertEqual(self.List.get(1), 'A')
+
+        self.List.insert('C', 2)
+        self.assertEqual(self.List.get(2), 'C')
+
+        self.List.insert('D', 1)
+        self.assertEqual(self.List.get(1), 'D')
+
+        with self.assertRaises(IndexError):
+            self.List.insert('E', -1)
+        with self.assertRaises(IndexError):
+            self.List.insert('E', 7)
