cd46f8bccbf4abf1c9bcd80916fe34eeec6f66c5
Yana <y.papish14@gmail.com>
2025-04-02 13:49:05 +0300

    add test_append method

--- arrDoublyLinkedList_test.py
+++ arrDoublyLinkedList_test.py
@@ -8,3 +8,12 @@ class TestArrayDoublyLinkedList(unittest.TestCase):
 
     def test_length(self):
         self.assertEqual(self.List.length(), 0)
+    
+    def test_append(self):
+        self.List.append('A')
+        self.assertEqual(self.List.length(), 1)
+        self.assertEqual(self.List.get(0), 'A')
+
+        self.List.append('B')
+        self.assertEqual(self.List.length(), 2)
+        self.assertEqual(self.List.get(1), 'B')
