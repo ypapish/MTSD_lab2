30ba882a9c47593c10e14d02cc3d8c4dc0437364
Yana <y.papish14@gmail.com>
2025-04-02 13:48:14 +0300

    add test_length method

--- arrDoublyLinkedList_test.py
+++ arrDoublyLinkedList_test.py
@@ -5,3 +5,6 @@ from arrDoublyLinkedList import arrayDoublyLinkedList
 class TestArrayDoublyLinkedList(unittest.TestCase):
     def setUp(self):
         self.List = arrayDoublyLinkedList()
+
+    def test_length(self):
+        self.assertEqual(self.List.length(), 0)
