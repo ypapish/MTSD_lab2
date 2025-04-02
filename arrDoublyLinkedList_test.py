ba08c2bdf1e057ee753c4f1cdf6bef190973e1c1
Yana <y.papish14@gmail.com>
2025-04-02 13:39:07 +0300

    create TestArrayDoublyLinkedList class

--- arrDoublyLinkedList_test.py
+++ arrDoublyLinkedList_test.py
@@ -1,2 +1,7 @@
 import unittest
 from arrDoublyLinkedList import arrayDoublyLinkedList
+
+
+class TestArrayDoublyLinkedList(unittest.TestCase):
+    def setUp(self):
+        self.List = arrayDoublyLinkedList()
